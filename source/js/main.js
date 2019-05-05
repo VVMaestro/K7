5ae5e2086202167414f9864a1b11270665aec5a6
Valentine <bmaecmpo@gmail.com>
2019-05-05 22:31:45 +0500

    Закончена главная страница

--- source/js/main.js
+++ source/js/main.js
@@ -5,7 +5,8 @@ var mainSwiper = new Swiper('.swiper-container', {
     loop: false,
     effect: 'slide',
     speed: 700,
-    allowTouchMove: false,
+    simulateTouch: false,
+    allowTouchMove: true,
 
     // If we need pagination
     pagination: {
@@ -34,10 +35,40 @@ var mainSwiper = new Swiper('.swiper-container', {
 
     on: {
         slideChange: function () {
+            const darkPages = [1];
             const nextArrow = document.querySelector('.swiper-button-next');
+            const prevArrow = document.querySelector('.swiper-button-prev');
+            const label = document.querySelector('.swiper-pagination-custom__label');
+            const switcher = document.querySelector('.main-nav__switch');
+            const pagination = document.querySelector('.swiper-pagination-custom');
+
+            //changing next arrow positionn on first slide
             if (mainSwiper.isBeginning) {
                 nextArrow.classList.add('swiper-button-next--on-start');
             } else nextArrow.classList.remove('swiper-button-next--on-start');
+
+            if (darkPages.includes(mainSwiper.activeIndex)) {
+                //changing both arrows and label color on dark slides
+                nextArrow.classList.add('swiper-button-next--white');
+                prevArrow.classList.add('swiper-button-prev--white');
+                label.classList.add('swiper-pagination-custom__label--white');
+
+                //changing menu-burger color on dark pages
+                switcher.classList.add('main-nav__switch--light');
+
+                //changing pagination color
+                pagination.classList.add('swiper-pagination-custom--light');
+            } else {
+                nextArrow.classList.remove('swiper-button-next--white');
+                prevArrow.classList.remove('swiper-button-prev--white');
+                label.classList.remove('swiper-pagination-custom__label--white');
+
+                switcher.classList.remove('main-nav__switch--light');
+
+                pagination.classList.remove('swiper-pagination-custom--light');
+            }
+
+            
         }        
     }
 
@@ -52,8 +83,26 @@ var innerSwiper = new Swiper ('.inner-swiper__container', {
     nested: true,
     slidesPerView: 1,
 
+    autoplay: {
+        delay: 5000
+    },
+
     pagination: {
         el: '.inner-swiper__pagination',
+        clickable: true
+    }
+});
+
+//Init info swiper
+var infoSwiper = new Swiper('.info-swiper__container', {
+    direction: 'vertical',
+    loop: false,
+    effect: 'slide',
+    speed: 700,
+    nested: true,
+
+    pagination: {
+        el: '.info-swiper__pagination',
         clickable: true
     }
 });
@@ -68,6 +117,18 @@ function onSwitcherClick () {
 
 switcher.addEventListener('click', onSwitcherClick);
 
+//Init intro-more button
+const moreButton = document.querySelector('.second-page__plus-button');
+const moreInfo = document.querySelector('.more-info');
+const closeButton = document.querySelector('.more-info__close-button');
+
+function onMoreButtomClick () {
+    moreInfo.classList.toggle('more-info--maxied');
+}
+
+moreButton.addEventListener('click', onMoreButtomClick);
+closeButton.addEventListener('click', onMoreButtomClick);
+
 //Init aside panel and small-logo
 mainSwiper.on('slideChange', function () {
     const asidePanel = document.querySelector('.page__aside-panel');
@@ -77,9 +138,39 @@ mainSwiper.on('slideChange', function () {
 });
 
 mainSwiper.on('slideChange', function () {
-    const pagesWithLogo = [1, 2];
+    const pagesWithLogo = [1];
     const logo = document.querySelector('.page__small-logo');
     if (pagesWithLogo.includes(mainSwiper.activeIndex)) {
         logo.classList.add('page__small-logo--show');
     } else logo.classList.remove('page__small-logo--show');
+});
+
+//Init second slide intro
+mainSwiper.on('slideChange', function () {
+    const PAGE_INDEX = 1;
+    const secPageIntro = document.querySelector('.second-page__intro');
+    const secPageIntroMore = document.querySelector('.more-info');
+
+    if (mainSwiper.activeIndex == PAGE_INDEX) {
+        secPageIntro.classList.add('second-page__intro--open');
+    } else {
+        secPageIntro.classList.remove('second-page__intro--open');
+        secPageIntroMore.classList.remove('more-info--maxied');
+    }
+});
+
+//init contact-popup
+const contactsForm = document.querySelector('#contacts-form');
+const popup = document.querySelector('.contacts-popup');
+const closePopup = document.querySelector('.contacts-popup__close');
+const darkBack = document.querySelector('.page__dark-back');
+
+contactsForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();//убрать после добавления action на форму!!!
+    popup.classList.add('contacts-popup--open');
+    darkBack.classList.add('page__dark-back--open');
+});
+closePopup.addEventListener('click', function () {
+    popup.classList.remove('contacts-popup--open');
+    darkBack.classList.remove('page__dark-back--open');    
 });
