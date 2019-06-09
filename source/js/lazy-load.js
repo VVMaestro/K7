76057d3aecf8e2c52b4714eec933da733439b59a
Valentine <bmaecmpo@gmail.com>
2019-06-10 00:53:14 +0500

    Улучшает ленивое появление блоков на странице.

--- source/js/lazy-load.js
+++ source/js/lazy-load.js
@@ -1,4 +1,9 @@
 (function () {
+    function hideElems(elems) {
+        elems.forEach(function (it) {
+            it.classList.add('js-lazy--hide');
+        });
+    } 
     function drawElems (elems) {
         elems.forEach(function (it) {
             let winHeight = document.documentElement.clientHeight;
@@ -11,6 +16,8 @@
 
     const lazyElems = document.querySelectorAll('.js-lazy');
     drawElems(lazyElems);
+
+    hideElems(lazyElems);
 
     window.addEventListener('scroll', function (evt) {
         drawElems(lazyElems);
