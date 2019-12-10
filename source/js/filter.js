60d5014fee4746743117562be73b67eef8190f53
Valentine <bmaecmpo@gmail.com>
2019-12-10 21:52:55 +0500

    Вносит изменения в вёрстку страницу с портфолио

--- source/js/filter.js
+++
@@ -1,7 +0,0 @@
-//init filter activation button
-const filterButton = document.querySelector('.filter__button');
-const filterWrapper = document.querySelector('.filter__wrapper');
-
-filterButton.addEventListener('click', function () {
-    filterWrapper.classList.toggle('filter__wrapper--active');
-});
